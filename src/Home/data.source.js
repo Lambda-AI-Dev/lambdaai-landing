103ed925685267a058ee2f672344661bd1b596cb
Daniel Tian <dtian.95@gmail.com>
2019-12-11 01:58:49 -0500

    fixed packages

--- src/Home/data.source.js
+++ src/Home/data.source.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 export const Banner50DataSource = {
-  wrapper: { className: 'home-page-wrapper banner5' },
-  page: { className: 'home-page banner5-page' },
+  wrapper: { className: "home-page-wrapper banner5" },
+  page: { className: "home-page banner5-page" },
   childWrapper: {
-    className: 'banner5-title-wrapper',
+    className: "banner5-title-wrapper",
     children: [
       {
-        name: 'title',
+        name: "title",
         children: (
           <>
             <p>
@@ -14,20 +14,20 @@ export const Banner50DataSource = {
             </p>
           </>
         ),
-        className: 'banner5-title k40vtjlgqe6-editor_css',
+        className: "banner5-title k40vtjlgqe6-editor_css"
       },
       {
-        name: 'explain',
-        className: 'banner5-explain k40x8v1v0tf-editor_css',
+        name: "explain",
+        className: "banner5-explain k40x8v1v0tf-editor_css",
         children: (
           <>
             <p>Affordable labeled data at scale</p>
           </>
-        ),
+        )
       },
       {
-        name: 'content',
-        className: 'banner5-content k40vtbnlt3m-editor_css',
+        name: "content",
+        className: "banner5-content k40vtbnlt3m-editor_css",
         children: (
           <>
             <p>
@@ -36,39 +36,41 @@ export const Banner50DataSource = {
               tasks.
             </p>
           </>
-        ),
+        )
       },
       {
-        name: 'button',
-        className: 'banner5-button-wrapper',
+        name: "button",
+        className: "banner5-button-wrapper",
         children: {
-          href: '#',
-          className: 'banner5-button k40vsmqkk3b-editor_css',
-          type: 'primary',
+          href: "#",
+          className: "banner5-button k40vsmqkk3b-editor_css",
+          type: "primary",
           children: (
             <>
               <p>Contact Us</p>
             </>
-          ),
-        },
-      },
-    ],
+          )
+        }
+      }
+    ]
   },
   image: {
-    className: 'banner5-image',
+    className: "banner5-image",
     children:
-      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-wAhRYnWQscAAAAAAAAAAABkARQnAQ',
-  },
+      "https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-wAhRYnWQscAAAAAAAAAAABkARQnAQ"
+  }
 };
 export const Footer00DataSource = {
-  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
-  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  wrapper: { className: "home-page-wrapper footer0-wrapper" },
+  OverPack: { className: "home-page footer0", playScale: 0.05 },
   copyright: {
-    className: 'copyright',
+    className: "copyright",
     children: (
       <>
-        <p>©2019 Lambda AI | Made with ❤️in Philadelphia</p>
+        <p>
+          ©2019 Lambda AI | Made with <span>❤️</span> in Philadelphia
+        </p>
       </>
-    ),
-  },
+    )
+  }
 };
